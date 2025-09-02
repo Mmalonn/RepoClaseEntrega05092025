5928905287ba42d3854b7c40ae970497c941375f
marcos torres <marcosezequiel_torres@hotmail.com>
2025-09-02 14:56:00 -0300

    terminada la logica para almacenar presupuestos con sus detalles

--- Data/BudgetRepository.cs
+++ Data/BudgetRepository.cs
@@ -34,9 +34,43 @@ namespace Repository2025.Data
             SqlTransaction t = null;
             try
             {
+                //trae instancia de la coneccion
                 cnn = DataHelper.GetInstance().GetConnection();
+                //la abre
                 cnn.Open();
-
+                //inicia transaccion
+                t = cnn.BeginTransaction();
+                //declara el comando para iniciar transacion
+                var cmd = new SqlCommand("SP_INSERTAR_MAESTRO", cnn, t);
+                //crea los parametros
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@cliente", budget.Cliente);
+                cmd.Parameters.AddWithValue("@vigencia", budget.Expiration);
+                //lineas para traer parametro que devuelve el store procedure y poder asignarle 
+                //el id del presupuesto a los detalles
+                SqlParameter param = new SqlParameter("@id", SqlDbType.Int);
+                param.Direction = ParameterDirection.Output;
+                cmd.Parameters.Add(param);
+                //ejecuta la query
+                cmd.ExecuteNonQuery();
+                //lee el parametro que trae el id
+                int budgetId = (int)param.Value;
+                //recorre la lista de detalles para meterlos a un sp
+                //que va insertar los detalles en el presupuesto anterior
+                int detailId = 1;
+                foreach (var detail in budget.Details)
+                {
+                    var cmdDetail = new SqlCommand("SP_INSERTAR_DETALLE", cnn, t);
+                    cmdDetail.CommandType = CommandType.StoredProcedure;
+                    cmdDetail.Parameters.AddWithValue("@detalle", detailId);
+                    cmdDetail.Parameters.AddWithValue("@presupuesto", budgetId);
+                    cmdDetail.Parameters.AddWithValue("@producto", detail.Product.Codigo);
+                    cmdDetail.Parameters.AddWithValue("@cantidad", detail.Count);
+                    cmdDetail.Parameters.AddWithValue("@precio", detail.Price);
+                    cmdDetail.ExecuteNonQuery();
+                    detailId++;
+                }
+                t.Commit();
             }
             catch (SqlException e)
             {
